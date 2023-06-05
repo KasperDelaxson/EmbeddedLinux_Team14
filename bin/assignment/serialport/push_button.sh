ccfed79d20b0d18f10e40ac9d3c118fb9a868a1a
KasperDelaxson <kasper_delaxson@hotmail.com>
2023-06-05 08:38:19 +0200

    Update push_button.sh
    Fixed wrong controls

--- bin/assignment/serialport/push_button.sh
+++ bin/assignment/serialport/push_button.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 serialnumber=$(cat serialport/SerialNumber.txt)
 while true; do
-button_count=$(curl -s http://192.168.10.222/button/a/count)
-echo $button_count
-if [[ $button_count -eq 2 ]]; then
-  bash publish_mqtt_message.sh "$serialnumber/pump" "p"
-  button_count=0
-fi
-sleep 2
+  initial_count=$(curl -s http://192.168.10.222/button/a/count)
+
+  if [[ $initial_count -eq 1 ]]; then
+    sleep 2
+    updated_count=$(curl -s http://192.168.10.222/button/a/count)
+    if [[ $updated_count -eq 0 ]]; then
+      bash publish_mqtt_message.sh "$serialnumber/pump" "p"
+    fi
+  fi
 done
+
+
